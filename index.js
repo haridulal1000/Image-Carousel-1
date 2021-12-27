0d7e44df63d44425b228778f85810bef5fed8290
haridulal1000 <84311145+haridulal1000@users.noreply.github.com>
2021-12-27 08:28:50 +0545

    removed double quotes

--- index.js
+++ index.js
@@ -2,91 +2,92 @@
 function carousel(mainId) {
   let dim = 500;
   let imageContainer = document.getElementById(mainId);
-  imageContainer.style.display = "block";
-  imageContainer.style.backgroundColor = "red";
-  imageContainer.style.width = dim + "px";
-  imageContainer.style.height = dim + "px";
-  imageContainer.style.margin = "0 auto";
-  imageContainer.style.position = "relative";
-  imageContainer.style.overflow = "hidden";
+  imageContainer.style.display = 'block';
+  imageContainer.style.backgroundColor = 'red';
+  imageContainer.style.width = dim + 'px';
+  imageContainer.style.height = dim + 'px';
+  imageContainer.style.margin = '0 auto';
+  imageContainer.style.position = 'relative';
+  imageContainer.style.overflow = 'hidden';
   const delay = 10;
   let pos = 0;
   let imageRow = document.querySelector(`#${mainId} div`);
-  imageRow.style.position = "relative";
-  imageRow.style.left = "0px";
+  imageRow.style.position = 'relative';
+  imageRow.style.left = '0px';
   let images = document.querySelectorAll(`#${mainId} div img`);
 
-  imageRow.style.width = images[0].getAttribute("width") + "px";
+  imageRow.style.width = images[0].getAttribute('width') + 'px';
 
-  imageContainer.style.width = images[0].getAttribute("width") + "px";
+  imageContainer.style.width = images[0].getAttribute('width') + 'px';
   let index = 0;
   for (let i = 0; i < images.length; i++) {
-    images[i].style.display = "block";
-    images[i].setAttribute("width", dim);
-    images[i].setAttribute("height", dim);
-    images[i].style.position = "absolute";
-    images[i].style.left = i * dim + "px";
+    images[i].style.display = 'block';
+    images[i].setAttribute('width', dim);
+    images[i].setAttribute('height', dim);
+    images[i].style.position = 'absolute';
+    images[i].style.left = i * dim + 'px';
   }
   //button-left
 
-  let buttonLeft = document.createElement("button");
-  buttonLeft.setAttribute("id", mainId + "btn-left");
-  buttonLeft.style.fontSize = "85px";
-  buttonLeft.innerHTML = "&#9001;";
-  buttonLeft.style.zIndex = "5";
-  buttonLeft.style.position = "absolute";
-  buttonLeft.style.backgroundColor = "transparent";
-  buttonLeft.style.color = "white";
-  buttonLeft.style.border = "none";
-  buttonLeft.style.top = "200px";
+  let buttonLeft = document.createElement('button');
+  buttonLeft.setAttribute('id', mainId + 'btn-left');
+  buttonLeft.style.fontSize = '85px';
+  buttonLeft.innerHTML = '&#9001;';
+  buttonLeft.style.zIndex = '5';
+  buttonLeft.style.position = 'absolute';
+  buttonLeft.style.backgroundColor = 'transparent';
+  buttonLeft.style.color = 'white';
+  buttonLeft.style.border = 'none';
+  buttonLeft.style.top = '200px';
   imageContainer.appendChild(buttonLeft);
 
   //button-right
-  let buttonRight = document.createElement("button");
-  buttonRight.setAttribute("id", mainId + "btn-right");
-  buttonRight.style.fontSize = "85px";
-  buttonRight.innerHTML = "&#9002;";
-  buttonRight.style.zIndex = "5";
-  buttonRight.style.position = "absolute";
-  buttonRight.style.backgroundColor = "transparent";
-  buttonRight.style.color = "white";
-  buttonRight.style.border = "none";
-  buttonRight.style.top = "200px";
-  buttonRight.style.left = "454px";
+  let buttonRight = document.createElement('button');
+  buttonRight.setAttribute('id', mainId + 'btn-right');
+  buttonRight.style.fontSize = '85px';
+  buttonRight.innerHTML = '&#9002;';
+  buttonRight.style.zIndex = '5';
+  buttonRight.style.position = 'absolute';
+  buttonRight.style.backgroundColor = 'transparent';
+  buttonRight.style.color = 'white';
+  buttonRight.style.border = 'none';
+  buttonRight.style.top = '200px';
+  buttonRight.style.left = '454px';
   imageContainer.appendChild(buttonRight);
   //dots
   let dots = [];
-  let dotsContainer = document.createElement("div");
-  dotsContainer.setAttribute("id", "dot-container");
-  dotsContainer.style.margin = "0 -15%";
-  dotsContainer.style.position = "absolute";
-  dotsContainer.style.zIndex = "10";
-  dotsContainer.style.top = "424px";
+  let dotsContainer = document.createElement('div');
+  dotsContainer.setAttribute('id', 'dot-container');
+  dotsContainer.style.margin = '0 -15%';
+  dotsContainer.style.position = 'absolute';
+  dotsContainer.style.zIndex = '10';
+  dotsContainer.style.top = '424px';
   dotsContainer.style.left = (70+(550-70)/(images.length-1))+'px';
   imageContainer.appendChild(dotsContainer);
   //creating dot elements
   for (let i = 0; i < images.length; i++) {
-    let dot = document.createElement("div");
+    let dot = document.createElement('div');
     dot.style.padding = `${50 / images.length}px`;
-    dot.style.display = "inline-block";
-    dot.style.margin = "10px";
-    dot.style.backgroundColor = "white";
-    dot.setAttribute("class", mainId + "dot" + i);
-    dot.style.borderRadius = "50%";
-    dot.addEventListener("click", handleDot);
+    dot.style.display = 'inline-block';
+    dot.style.margin = '10px';
+    dot.style.backgroundColor = 'white';
+    dot.setAttribute('id', mainId + 'dot' + i);
+    dot.setAttribute('class', 'dots');
+    dot.style.borderRadius = '50%';
+    dot.addEventListener('click', handleDot);
     dots.push(dot);
     dotsContainer.appendChild(dots[i]);
-    dots[0].style.backgroundColor = "red";
+    dots[0].style.backgroundColor = 'red';
   }
 
   //adding event-listeners to the buttons
-  buttonLeft.addEventListener("click", shift);
-  buttonRight.addEventListener("click", shift);
+  buttonLeft.addEventListener('click', shift);
+  buttonRight.addEventListener('click', shift);
   function shift(e) {
     pos = parseInt(imageRow.style.left);
-    if (e.target.getAttribute("id") === mainId + "btn-left") {
+    if (e.target.getAttribute('id') === mainId + 'btn-left') {
       shiftRight(delay);
-    } else if (e.target.getAttribute("id") === mainId + "btn-right") {
+    } else if (e.target.getAttribute('id') === mainId + 'btn-right') {
       shiftLeft(delay);
     }
   }
@@ -102,12 +103,12 @@ function carousel(mainId) {
     leftShifting = window.requestAnimationFrame(() => shiftLeft(d));
     if (pos <= (index - 1) * dim) {
       pos = (index - 1) * dim;
-      imageRow.style.left = pos + "px";
+      imageRow.style.left = pos + 'px';
       window.cancelAnimationFrame(leftShifting);
       index -= 1;
     } else {
       pos -= d;
-      imageRow.style.left = pos + "px";
+      imageRow.style.left = pos + 'px';
     }
     colorizeDot(dots[-index]);
   }
@@ -121,12 +122,12 @@ function carousel(mainId) {
     rightShifting = window.requestAnimationFrame(() => shiftRight(d));
     if (pos >= (index + 1) * dim) {
       pos = (index + 1) * dim;
-      imageRow.style.left = pos + "px";
+      imageRow.style.left = pos + 'px';
       window.cancelAnimationFrame(rightShifting);
       index += 1;
     } else {
       pos += d;
-      imageRow.style.left = pos + "px";
+      imageRow.style.left = pos + 'px';
     }
     colorizeDot(dots[-index]);
   }
@@ -134,7 +135,7 @@ function carousel(mainId) {
   //handling clicks on the dots
   function handleDot(e) {
     let ind = parseInt(
-      e.target.getAttribute("class").slice(mainId.length + 3)
+      e.target.getAttribute('id').slice(mainId.length + 3)
     );
     let diff = -ind - index;
     if (diff < 0) {
@@ -151,12 +152,12 @@ function carousel(mainId) {
   //colorizing the dots
   function colorizeDot(e) {
     for (let i = 0; i < dots.length; i++) {
-      dots[i].style.backgroundColor = "white";
+      dots[i].style.backgroundColor = 'white';
     }
-    e.style.backgroundColor = "red";
+    e.style.backgroundColor = 'red';
   }
 }
 
 
-carousel("image-container");
-carousel("image-container2");
+carousel('image-container');
+carousel('image-container2');
